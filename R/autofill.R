cacaf063479bb77f0f07a62936cd656b6a9e9fb1
Cory Merow <cory.merow@gmail.com>
2017-11-29 15:09:02 -0500

    trying to auto cite

---
+++ R/autofill.R
@@ -0,0 +1,26 @@
+#' @title
+#'
+#' @description
+#'
+#' @details
+#' See Examples.
+#'
+#' @param
+#' @export
+#'
+# @examples
+#'
+#'
+#' @return
+#' @author Cory Merow <cory.merow@@gmail.com>, Brian Maitner <bmaitner@@gmail.com>,
+#' @note
+# @seealso
+# @references
+# @aliases - a list of additional topic names that will be mapped to
+# this documentation when the user looks them up from the command
+# line.
+# @family - a family name. All functions that have the same family tag will be linked in the documentation.
+packages=c('raster','sp')
+rmmPackageCitation=function(packages){
+  lapply(packages,function(x) toBibtex(citation(x)))
+}
--- R/metadataTemplate.R
+++ R/metadataTemplate.R
@@ -9,8 +9,9 @@
 #' @export
 #'
 #' @examples
-#' speciesRangeMetadataTemplate()
-#' speciesRangeMetadataTemplate(obgligateOnly=T)
+#' rmm1=rangeModelMetadataTemplate()
+#' rmm2=rangeModelMetadataTemplate(obgligateOnly=T)
+#' str(rmm2)
 #'
 #' @return
 #' @author Cory Merow <cory.merow@@gmail.com>, Brian Maitner <bmaitner@@gmail.com>
@@ -22,39 +23,39 @@
 # line.
 # @family - a family name. All functions that have the same family tag will be linked in the documentation.
 
-speciesRangeMetadataTemplate=function(obligateOnly=TRUE){
+rangeModelMetadataTemplate=function(obligateOnly=TRUE){
 
   dd=read.csv(system.file("extdata/dataDictionary.csv",package='rangeModelMetaData'))
 
   #== Level 1 fields
   field1=as.character(unique(dd$Field1))
-  mdo=sapply(field1,function(x) NULL)
+  rmm=sapply(field1,function(x) NULL)
 
   #== Level 2-4 fields
-  for(i in 1:length(mdo)){
+  for(i in 1:length(rmm)){
     if(obligateOnly){
-      dd.f2=subset(dd,Field1==names(mdo)[i] & Obligate==1)
-    } else {dd.f2=subset(dd,Field1==names(mdo)[i])}
+      dd.f2=subset(dd,Field1==names(rmm)[i] & Obligate==1)
+    } else {dd.f2=subset(dd,Field1==names(rmm)[i])}
     field2=as.character(unique(dd.f2$Field2))
-    mdo[[i]]=sapply(field2,function(x) NULL)
+    rmm[[i]]=sapply(field2,function(x) NULL)
     for(j in 1:length(field2)){
       if(obligateOnly){
-        dd.f3=subset(dd.f2,Field2==names(mdo[[i]])[j]  & Obligate==1)
-      } else { dd.f3=subset(dd.f2,Field2==names(mdo[[i]])[j]) }
+        dd.f3=subset(dd.f2,Field2==names(rmm[[i]])[j]  & Obligate==1)
+      } else { dd.f3=subset(dd.f2,Field2==names(rmm[[i]])[j]) }
       field3=as.character(unique(dd.f3$Field3))
       if(!all(is.na(field3))){
-        mdo[[i]][[j]]=sapply(field3,function(x) NULL)
+        rmm[[i]][[j]]=sapply(field3,function(x) NULL)
         for(k in 1:length(field3)){
-          if(obligateOnly){ dd.f4=subset(dd.f3,Field3==names(mdo[[i]][[j]])[k] & Obligate==1)
-          } else {  dd.f4=subset(dd.f3,Field3==names(mdo[[i]][[j]])[k]) }
+          if(obligateOnly){ dd.f4=subset(dd.f3,Field3==names(rmm[[i]][[j]])[k] & Obligate==1)
+          } else {  dd.f4=subset(dd.f3,Field3==names(rmm[[i]][[j]])[k]) }
           field4=as.character(unique(dd.f4$Field4))
-          if(!all(is.na(field4))) mdo[[i]][[j]][[k]]=sapply(field4,function(x) NULL)
+          if(!all(is.na(field4))) rmm[[i]][[j]][[k]]=sapply(field4,function(x) NULL)
         }
       }
     }
   }
 
-  mdo
+  rmm
 }
 
 
