80904d8ff03afa7e4d7edc35041a8465322d403f
bmaitner <bmaitner@gmail.com>
2017-11-30 07:22:49 -0700

    rmmPackageCitation updated, works properly in test cases

--- R/autofill.R
+++ R/autofill.R
@@ -24,3 +24,19 @@
 # rmmPackageCitation=function(packages){
 #   lapply(packages,function(x) toBibtex(citation(x)))
 # }
+rmmPackageCitation=function(packages){
+  out<-lapply(packages,function(x) toBibtex(citation(x)))
+  out<-unlist(out)
+  out<-paste0(out,collapse = "")
+  #The following bit could be done in a more clever way with regex, but this works for now
+  out<-strsplit(x = out,split = "}@")
+  out<-out[[1]]
+  if(length(out)>1){
+
+    out[2:length(out)]<-paste0("@",out[2:length(out)])
+    out[1:(length(out))-1]<-paste0(out[1:(length(out))-1], "}" )
+  }
+
+  return(out)
+
+}
