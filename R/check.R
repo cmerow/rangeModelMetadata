67de9247df1873e978e8cbb9e95a5b8b4d4efd0c
bmaitner <bmaitner@gmail.com>
2017-11-30 08:58:55 -0700

    rmmNameCheck function updated and working

--- R/check.R
+++ R/check.R
@@ -26,7 +26,7 @@ rmmCleanEmpties=function(rmm){
 }
 
 
-#' @title Check field names of a range model metadata list against convections
+#' @title Check field names of a range model metadata list against conventions
 #'
 #' @description Identify nonstandard fields
 #'
@@ -39,16 +39,31 @@ rmmCleanEmpties=function(rmm){
 # @examples
 #'
 #'
-#' @return
+#' @return A vector of names that are not found in the range model metadata dictionary.
 #' @author Cory Merow <cory.merow@@gmail.com>, Brian Maitner <bmaitner@@gmail.com>,
-#' @note
+#' @note Names returned by this check may be either incorrectly named or correctly named but missing from the data dictionary.
 # @seealso
 # @references
 # @aliases - a list of additional topic names that will be mapped to
 # this documentation when the user looks them up from the command
 # line.
 # @family - a family name. All functions that have the same family tag will be linked in the documentation.
-
 rmmNameCheck=function(rmm){
+
+  #nametree function by Vincent Zoonekynd
+  nametree <- function(X, prefix = "")
+    if( is.list(X) )
+      for( i in seq_along(X) ) {
+        cat( prefix, names(X)[i], "\n", sep="" )
+        nametree(X[[i]], paste0(prefix, ""))
+      }
+
+  list_names<-capture.output(nametree(rmm))
+  dd=read.csv(system.file("extdata/dataDictionary.csv",package='rangeModelMetaData'), stringsAsFactors = T)
+  accepted_names<-c(as.character(dd$Field1), as.character(dd$Field2), as.character(dd$Field3), as.character(dd$Field4))
+
+  questionable_names <- list_names[which(!list_names%in%accepted_names)]
+
+  return(questionable_names)
 
 }
