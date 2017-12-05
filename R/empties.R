3a3c6d1ac4cff281b160af1e75cef925aa3d8972
Cory Merow <cory.merow@gmail.com>
2017-12-05 12:23:32 -0500

    random vignette stuff

---
+++ R/empties.R
@@ -0,0 +1,26 @@
+#' @title Clean unused fields from range model metadata list
+#'
+#' @description Only optional fields that are empty are removed.
+#'
+#' @details
+#' See Examples.
+#'
+#' @param rmm a range model metadata list
+#' @export
+#'
+# @examples
+#'
+#'
+#' @return
+#' @author Cory Merow <cory.merow@@gmail.com>, Brian Maitner <bmaitner@@gmail.com>,
+#' @note
+# @seealso
+# @references
+# @aliases - a list of additional topic names that will be mapped to
+# this documentation when the user looks them up from the command
+# line.
+# @family - a family name. All functions that have the same family tag will be linked in the documentation.
+
+rmmFindEmpties=function(rmm){
+
+}
